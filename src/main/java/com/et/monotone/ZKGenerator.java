a1282abf0df873274597af4ce2bcbb78f4255efe
Mark Greene <mark@evertrue.com>
2014-10-28 17:18:37 -0400

    retry logic on optimistic failures + code cleanup

--- src/main/java/com/et/monotone/ZKGenerator.java
+++ src/main/java/com/et/monotone/ZKGenerator.java
@@ -2,6 +2,7 @@ package com.et.monotone;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
 import com.google.common.collect.Range;
 import com.netflix.curator.RetryPolicy;
@@ -11,24 +12,30 @@ import com.netflix.curator.framework.recipes.atomic.DistributedAtomicLong;
 import com.netflix.curator.retry.RetryNTimes;
 
 public class ZKGenerator {
+	private final static int MAX_ATTEMPTS = 5;
 	private Range<Long> idRange;
 	private final AtomicLong idCounter;
 	private int maxIdsToFetch;
-	private CuratorFramework client;
 	private DistributedAtomicLong zkCounter;
 
 	private ZKGenerator(CuratorFramework client, String rootPath, String counterName, int maxIdsToFetch) {
 		this.idCounter = new AtomicLong(0);
 		this.idRange = Range.closedOpen(0l, 0l);
 		this.maxIdsToFetch = maxIdsToFetch;
+
 		RetryPolicy policy = new RetryNTimes(1, 100);
 		this.zkCounter = new DistributedAtomicLong(client, rootPath + "/" + counterName, policy);
+		
+		initValueIfNeeded();
+	}
+
+	private void initValueIfNeeded() {
 		try {
 			if (this.zkCounter.get().postValue() == 0) {
 				this.zkCounter.trySet(0l);
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			Throwables.propagate(e);
 		}
 	}
 
@@ -53,16 +60,17 @@ public class ZKGenerator {
 
 	private void refreshAndSetNewRange() {
 		synchronized (idCounter) {
-			if (!idRange.contains(idCounter.get())) {
-				try {
-					System.out.println("Hitting ZK");
-					Thread.sleep(5000);
+			if (idRange.contains(idCounter.get())) {
+				return;
+			}
+
+			try {
+				for (int i = 0; i < MAX_ATTEMPTS; i++) {
 					// Get latest count from ZK counter
 					AtomicValue<Long> val = zkCounter.get();
 					long currentZkCount = val.postValue();
 					long newCount = currentZkCount + maxIdsToFetch;
 
-					
 					// Increment by maxIdsToFetch and write back
 					AtomicValue<Long> result = zkCounter.compareAndSet(currentZkCount, newCount);
 
@@ -70,14 +78,11 @@ public class ZKGenerator {
 					if (result.succeeded()) {
 						idRange = Range.closed(currentZkCount, newCount);
 						idCounter.set(currentZkCount + 1);
-					} else {
-						System.err.println("DId not succeed");
+						return;
 					}
-
-					System.out.println("Hitting ZK Done");
-				} catch (Exception e) {
-					Throwables.propagate(e);
 				}
+			} catch (Exception e) {
+				Throwables.propagate(e);
 			}
 		}
 	}
