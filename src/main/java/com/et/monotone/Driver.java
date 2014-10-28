2ec0a13eebee718ba109297ee200e173d6b724cd
Mark Greene <mark@evertrue.com>
2014-10-28 18:15:15 -0400

    fix compile errors

--- src/main/java/com/et/monotone/Driver.java
+++ src/main/java/com/et/monotone/Driver.java
@@ -1,5 +1,6 @@
 package com.et.monotone;
 
+import com.et.monotone.zk.ZKGenerator;
 import com.netflix.curator.RetryPolicy;
 import com.netflix.curator.framework.CuratorFramework;
 import com.netflix.curator.framework.CuratorFrameworkFactory;
@@ -11,15 +12,15 @@ public class Driver {
 		RetryPolicy policy = new RetryNTimes(1, 100);
 		CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", 3000, 3000, policy);
 		client.start();
-		ZKGenerator gen = ZKGenerator.newBuilder(client).setCounterName("mark_test").setMaxIdsToFetch(50)
+		IdGenerator gen = ZKGenerator.newBuilder(client).setCounterName("mark_test").setMaxIdsToFetch(1000)
 				.setRootPath("/id_gen").build();
 
 		System.out.println("Starting");
 		long start = System.currentTimeMillis();
-		for (int i = 0; i < 5000; i++) {
+		for (int i = 0; i < 50000; i++) {
 			
 			long id = gen.nextId();
-			System.out.println(id);
+//			System.out.println(id);
 		}
 
 		System.out.println("Took : " + String.valueOf(System.currentTimeMillis() - start));
